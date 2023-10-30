84a156727f168f6f537283fc33539451c8132c6f
persme1111 <bo12@illinois.edu>
2023-10-29 21:59:40 -0700

    update text preprocessing
    update requirements.txt

---
+++ src/preprocessing/text_preprocessing.py
@@ -0,0 +1,33 @@
+import torch
+import numpy
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import PorterStemmer
+
+torch.device('cpu')
+print(torch.Tensor([1, 2, 3]))
+
+nltk.download('stopwords')
+nltk.download('punkt')
+
+
+def preprocess_text(text):
+    # Tokenize the text
+    words = word_tokenize(text)
+
+    # Initialize the stemmer
+    stemmer = PorterStemmer()
+
+    # Remove stopwords and apply stemming
+    filtered_words = [stemmer.stem(word) for word in words if word.lower() not in set(stopwords.words('english'))]
+
+    # Join the filtered words back into a string
+    preprocessed_text = ' '.join(filtered_words)
+
+    return preprocessed_text
+
+
+text = "Text preprocessing in PyTorch involves stemming and removing stop words."
+preprocessed__text = preprocess_text(text)
+print(preprocessed__text)
